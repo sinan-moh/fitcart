f3c34370ad0135347835f80bbaddb1831c5593c7
Sinan <sinan956733@gmail.com>
2026-05-11 12:52:51 +0530

    passport update commit

--- config/passport.js
+++ config/passport.js
@@ -1,31 +1,38 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/userSchema");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://fitcart.site/auth/google/callback", // Ensure this matches in Google Cloud Console
+
+      callbackURL:
+        process.env.NODE_ENV === "production"
+          ? "https://fitcart-omega.vercel.app/auth/google/callback"
+          : "http://localhost:5000/auth/google/callback",
     },
+
     async (accessToken, refreshToken, profile, done) => {
       try {
         let user = await User.findOne({ googleId: profile.id });
+
         if (user) {
           return done(null, user);
-        } else {
-          user = new User({
-            name: profile.displayName,
-            email: profile.emails[0].value, // Fix property access
-            googleId: profile.id, // Fix camel case
-          });
-          await user.save();
-          return done(null, user);
         }
+
+        user = new User({
+          name: profile.displayName,
+          email: profile.emails?.[0]?.value,
+          googleId: profile.id,
+        });
+
+        await user.save();
+        return done(null, user);
       } catch (error) {
-        return done(error, null); // Correct error handling
+        return done(error, null);
       }
     }
   )
@@ -35,14 +42,13 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then((user) => {
-      done(null, user);
-    })
-    .catch((err) => {
-      done(err, null);
-    });
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 module.exports = passport;
